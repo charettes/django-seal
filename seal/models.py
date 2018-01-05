9017db7617d8e440132f986df07b6d37e52d7448
Bryan Helmig <bryan@zapier.com>
2018-01-04 20:47:23 -0500

    Add support for many-to-many reverse relations.

--- seal/models.py
+++ seal/models.py
@@ -4,7 +4,8 @@ from django.utils.six import with_metaclass
 from .exceptions import SealedObject
 from .managers import SealableQuerySet
 from .related import (
-    create_sealable_m2m_contribute_to_class, sealable_accessor_classes,
+    create_sealable_m2m_contribute_to_class,
+    create_sealable_m2m_contribute_to_related_class, sealable_accessor_classes,
 )
 
 
@@ -20,9 +21,11 @@ class SealaleModelBase(models.base.ModelBase):
                     value.forward_related_accessor_class = sealable_forward_accessor_class
             elif isinstance(value, models.ManyToManyField):
                 # ManyToManyField doesn't declare a class level attribute for
-                # its forward related accessor class. We must provide override
-                # contribute_to_class to work around it.
+                # its forward and reverse accessor class. We must provide
+                # override contribute_to_class/contribute_to_related_class to
+                # work around it.
                 value.contribute_to_class = create_sealable_m2m_contribute_to_class(value)
+                value.contribute_to_related_class = create_sealable_m2m_contribute_to_related_class(value)
         return super(SealaleModelBase, cls).__new__(cls, name, bases, attrs)
 
 
