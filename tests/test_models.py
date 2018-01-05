908f1a34519570a7723ceaddd6607a0e3cdb47c7
Simon Charette <simon.charette@zapier.com>
2018-01-04 20:57:10 -0500

    Adjust a test to always have a local id.

--- tests/test_models.py
+++ tests/test_models.py
@@ -20,7 +20,7 @@ class SealableModelTests(SimpleTestCase):
             instance.location
 
     def test_sealed_instance_reverse_foreign_key_access(self):
-        instance = Location.from_db('default', ['latitude', 'longitude'], [1.2, 3.4])
+        instance = Location.from_db('default', ['id'], [1])
         instance._state.sealed = True
         message = "Cannot fetch many-to-many field visitors on a sealed object."
         with self.assertRaisesMessage(SealedObject, message):
