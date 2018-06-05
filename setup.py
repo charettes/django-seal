12546c5ae335ba480251d570c044468a9c947435
Simon Charette <charette.s@gmail.com>
2018-06-05 00:02:40 -0400

    Release 1.0.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-seal',
-    version='1.0.0rc9',
+    version='1.0.0',
     description=(
         'Allows ORM constructs to be sealed to prevent them from executing '
         'queries on attribute accesses.'
