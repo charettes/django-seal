9bc49c7d4a64d0c44f2fb10f0eaad93312cc8f64
Simon Charette <simon.charette@zapier.com>
2020-02-23 23:10:48 -0500

    Bumped version number to 1.2.3a0 for development.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-seal',
-    version='1.2.2',
+    version='1.2.3a0',
     description=(
         'Allows ORM constructs to be sealed to prevent them from executing '
         'queries on attribute accesses.'
